4b31edf1d0190848076fe842efec575f6533f248
unknown <maodeh@asaltech.com>
2022-11-08 23:53:58 +0200

    edit burger menu

--- src/components/Header/BurgerMenu/index.js
+++ src/components/Header/BurgerMenu/index.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.css";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 const BurgerMenu = () => {
+  const [flag, setFlag] = useState(false);
+  function HandelMenuOnClick() {
+    setFlag(!flag);
+  }
   const menuItems = ["Login", "shop", "fabric", "journal", "About"];
 
   return (
     <div className={styles.BurgerMenu}>
       <img
+        onClick={HandelMenuOnClick}
         className={styles.BurgerMenuIcon}
-        src={"/icons/icon-hamburger.svg"}
+        src={flag ? "/icons/close.png" : "/icons/icon-hamburger.svg"}
         alt="Burger menu"
       />
+      {flag && (
+        <ul className={styles.BurgerMenuList}>
+          {menuItems.map((item) => {
+            return (
+              <li className={styles.MenuItem}>
+                <a>
+                  {" "}
+                  {item}
+                 <span  className={styles.ArrowIcon}><MdKeyboardArrowRight /></span> 
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
